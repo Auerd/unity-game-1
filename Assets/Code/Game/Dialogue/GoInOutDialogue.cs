646e909fb4f23efd3e4e98564a8e973912556f2a
Auerd <yaroslsvbychkov10@gmail.com>
2022-11-25 14:40:59 +0400

    Dialogue function was added

---
+++ Assets/Code/Game/Dialogue/GoInOutDialogue.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Awake()
+    {
+        GlobalEventManager.OnDialogEnded.AddListener(StartAnimationIn);
+        GlobalEventManager.OnDialogEnded.AddListener(StartAnimationOut);
+    }
+
+    private void StartAnimationIn()
+    {
+        animator.SetTrigger("StartDialogue");
+    }
+
+    private void StartAnimationOut()
+    {
+        animator.SetTrigger("EndDialogue");
+    }
+}
--- Assets/Code/Game/GlobalEventManager/GlobalEventManager.cs
+++ Assets/Code/Game/GlobalEventManager/GlobalEventManager.cs
@@ -2,18 +2,31 @@ using UnityEngine.Events;
 
 public class GlobalEventManager
 {
-    // Pressing pause event
-    public static UnityEvent OnPausePressed = new UnityEvent();
-    public static void SendPausePressed()
-    {
-        if (OnPausePressed != null) OnPausePressed.Invoke();
-    }
+	// Pressing pause event
+	public static UnityEvent OnPausePressed = new UnityEvent();
+	public static void SendPausePressed()
+	{
+		if (OnPausePressed != null) OnPausePressed.Invoke();
+	}
 
 
-    // Unpressing pause event
-    public static UnityEvent OnPauseUnpressed = new UnityEvent();
-    public static void SendPauseUnpressed()
-    {
-        if (OnPauseUnpressed != null) OnPauseUnpressed.Invoke();
-    }
+	// Unpressing pause event
+	public static UnityEvent OnPauseUnpressed = new UnityEvent();
+	public static void SendPauseUnpressed()
+	{
+		if (OnPauseUnpressed != null) OnPauseUnpressed.Invoke();
+	}
+
+	// Dialogue
+	public static UnityEvent OnDialogStarted = new UnityEvent();
+	public static void SendDialogStarted()
+	{
+		if (OnDialogStarted != null) OnDialogStarted.Invoke();
+	}
+
+	public static UnityEvent OnDialogEnded = new UnityEvent();
+	public static void SendDialogEnded()
+	{
+		if (OnDialogEnded != null) OnDialogEnded.Invoke();
+	}
 }
