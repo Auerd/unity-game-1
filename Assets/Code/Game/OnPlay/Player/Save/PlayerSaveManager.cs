bbfc00506cdf228c38d23ff8938cc7709aa14a3f
Auerd <yaroslsvbychkov10@gmail.com>
2023-03-03 18:19:30 +0400

    Serialization was redid

---
+++ Assets/Code/Game/OnPlay/Player/Save/PlayerSaveManager.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+
+public class PlayerSaveManager : MonoBehaviour
+{
+	string saveKey;
+
+	private void Start()
+	{
+		saveKey = transform.name;
+		GlobalEventManager.OnSavePressed.AddListener(Save);
+		Load();
+	}
+
+    private void OnApplicationQuit()
+    {
+        Save();
+    }
+
+    [Serializable]
+	private struct PlayerData
+	{
+        public float[] pos;
+		public int lvl;
+    }
+
+	void Save()
+	{
+		PlayerData data = new()
+		{
+			pos = new float[2]
+			{
+				transform.position.x, transform.position.y,
+			},
+		};
+		SaveManager.Save(saveKey, data);
+	}
+
+	void Load()
+	{
+		if(SaveManager.TryLoad(saveKey, out PlayerData data))
+		{
+			transform.position = new Vector2()
+			{
+				x = data.pos[0],
+				y = data.pos[1]
+			};
+		}
+	}
+}
